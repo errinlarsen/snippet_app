3e711e1c1107ccfbcaede1c1e2360dc4d69534ee
Errin Larsen <elarsen@insightcards.com>
2011-03-05 15:32:10 -0600

    [002] Get index spec working

--- spec/views/snippet/index.html.haml_spec.rb
+++ spec/views/snippet/index.html.haml_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
-describe "snippet/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "snippet/index.html.haml", :type => :request do
+  it "displays a welcome message" do
+    visit '/'
+    page.should have_content( 'Welcome to Snippets!' )
+  end
 end
