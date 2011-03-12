36ca7b28b875d8651ec7edfbe95edfdb9b977449
Errin Larsen <elarsen@insightcards.com>
2011-03-12 11:44:40 -0600

    [002] Add SnippetsController spec; also clean up quoting and method calls

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,5 +1,11 @@
 class SnippetsController < ApplicationController
+  def index
+  end
+
   def create
+    snippet = Snippet.new(params[:snippet])
+    snippet.save
+    redirect_to :action => "index"
   end
 
 end
--- spec/controllers/snippets_controller_spec.rb
+++ spec/controllers/snippets_controller_spec.rb
@@ -2,12 +2,35 @@ require 'spec_helper'
 
 describe SnippetsController do
 
+  let(:snippet) { mock_model(Snippet).as_null_object }
+  let(:snippet_params) {
+    { "name" => "A snippet",
+    "type" => "1",
+    "description" => "A snippet's description",
+    "tags" => "Test Tag",
+    "text" => "a quick brown fox" }
+  }
+
+  before do
+    Snippet.stub(:new).and_return(snippet)
+  end
+  
+
   describe "POST 'create'" do
-    it 'creates a new snippet' do
-      Snippet.should_receive(:new).with("text" => "a quick brown fox")
-      post :create, :snippets => { "text" => "a quick brown fox" }
+    it "creates a new snippet" do
+      Snippet.should_receive(:new).with(snippet_params).and_return(snippet)
+      post :create, :snippet => snippet_params
     end      
-    it 'saves the snippet'
+
+    it "saves the snippet" do
+      snippet.should_receive(:save)
+      post :create
+    end
+    
+    it "redirects to the Snippets index" do
+      post :create
+      response.should redirect_to(:action => "index")
+    end
   end
 
 end
