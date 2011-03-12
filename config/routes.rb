66eea565c0ef9313838986b3f1c0d80d93eeda78
Errin Larsen <elarsen@insightcards.com>
2011-03-12 10:09:34 -0600

    [002] Make the homepage a new.html.haml form

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ SnippetApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "snippet#index"
+  root :to => "snippet#new"
 
   # See how all your routes lay out with "rake routes"
 
--- spec/views/snippet/new.html.haml_spec.rb
+++ spec/views/snippet/new.html.haml_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "snippet/index.html.haml" do
+describe "snippet/new.html.haml" do
   #  The following will be useful soon ...
   #  
 #  let( :lua_snippet ) do
