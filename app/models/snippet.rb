0faa4ea1c398a84f6e4ab0ee79a78d2bbde0a767
Errin Larsen <elarsen@insightcards.com>
2011-03-12 22:22:45 -0600

    [002] Add attributes to Snippet model; used rails generate model cmd
    $ rails generate model snippet name:string type:string
    description:string tags:string text:text      invoke  mongoid
          create    app/models/snippet.rb
          invoke    rspec
       identical      spec/models/snippet_spec.rb

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,3 +1,8 @@
 class Snippet
   include Mongoid::Document
+  field :name, :type => String
+  field :type, :type => String
+  field :description, :type => String
+  field :tags, :type => String
+  field :text, :type => String
 end
