850b970accb265dce2a2a710f3877c9ecea06098
Charles Jarrett <jarrett.charles@gmail.com>
2011-03-16 22:41:20 -0500

    [003] Fix name collision

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,7 +1,7 @@
 class Snippet
   include Mongoid::Document
   field :name, :type => String
-  field :type, :type => String
+  field :category, :type => String
   field :description, :type => String
   field :tags, :type => String
   field :text, :type => String
